fe1c7c9133df23920162c3645976274e197333aa
Jon <jonathanwheeler8@gmail.com>
2017-02-14 21:42:07 +0000

    Backbone of main file started

---
+++ main.sh
@@ -0,0 +1,107 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: main.sh
+# 
+#         USAGE: ./main.sh -y year -e email (o)-u user (o)-p password
+# 
+#   DESCRIPTION: Main wrapper for homework 4
+# 
+#         NOTES: ---
+#        AUTHOR: Jon Wheeler (), jonathanwheeler1@weber.edu
+#       CREATED: 02/14/2017 21:16
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+help()
+{
+	echo "Usage ./$0 -y YEAR -e EMAIL (o)-u USER (o)-p PASSWD"
+	exit 1;
+}
+if [[ $1 == "--help" ]]
+then
+	help
+fi
+
+while getopts ":y:e:u:p:" opt
+do
+	case $opt in
+		y) year=$OPTARG
+			;;
+		e) email=$OPTARG
+			;;
+		u) user=$OPTARG
+			;;
+		p) passwd=$OPTARG
+			;;
+		/?)
+			help
+			;;
+	esac
+done
+
+if [[ -z $year ]]
+then
+	echo "Require Year"
+	help
+fi
+
+if [[ -z $email ]]
+then
+	echo "Require email"
+	help
+fi
+
+if [[ !-z $user ]] && [[ !-z $passwd ]]
+then 
+	echo "Pulling files with $user"
+else
+	echo "Pulling files with anonymous"
+fi
+
+# check proper exit from pulling files
+if [[ $? == 1 ]]
+then
+	echo "Error in pulling files"
+	exit 1
+fi
+
+
+
+# run expandfiles script
+
+if [[ $? == 1 ]]
+then
+	echo "Error in expanding files"
+	exit 1
+fi
+
+# call filterscript
+if [[ $? == 1 ]]
+then
+	echo "Error in filtering data"
+	exit 1
+fi
+
+
+# run FTP script
+if [[ $? == 1 ]]
+then
+	echo "Error in FTP Script"
+	exit 1
+fi
+
+
+# run clean script
+if [[ $? == 1 ]]
+then
+	echo "Error in cleaning temp files."
+	exit 1
+fi
+
+echo "Process finished"
+
+
+exit 0
